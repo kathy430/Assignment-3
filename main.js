2cef4cfcdf128488a3b32252317d811ea2a0a49f
kathy <itscathywithak@gmail.com>
2023-05-30 23:18:17 -0700

    adjust main to clear background to black

--- main.js
+++ main.js
@@ -5,5 +5,14 @@ function init() {
     // clear canvas to black
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
-    cube = new Cube(gl, )
+    //cube = new Cube(gl, )
+    
+    render()
 }
+
+function render() {
+    // clear canvas
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
+window.onload = init;
